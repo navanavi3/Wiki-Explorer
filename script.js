67c66a55ec74b970eb5607854761a63f454b8666
navindu amarakoon <navanav3@gmail.com>
2018-10-22 12:41:34 +0530

    wabbba lubba dub dub

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
-
-function search(){
+function search() {
   var query = $("#query").val();
   $("#result").html("");
-  if (query == "" || query == " " || query==""){
-    alert("Enter a something to search dummy!")
-  }else{
-  $.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&gsrlimit=15&generator=search&origin=*&gsrsearch=" + query, function(data){  //get JSON data from Wiki
-    console.log(data);
-    $("#result").append("<h4>Top 15 Search results for \"" + query +"\"</h4>");
-    $.each(data.query.pages, function(i){
-      var thing = data.query.pages;
-      $("#result").append("<li class=\"mdl-list__item\"><a target=\"_blank\" href=\"https://en.wikipedia.org/?curid="+
-            thing[i].pageid +
-          "\"<span class=\"mdl-list__item-primary-content\"><i class=\"material-icons mdl-list__item-icon\">link</i>" +
+  if (query == "" || query == " " || query == "") {
+    alert("Enter a something to search dummy!");
+  } else {
+    $.getJSON(
+      "https://en.wikipedia.org/w/api.php?action=query&format=json&gsrlimit=15&generator=search&origin=*&gsrsearch=" +
+        query,
+      function(data) {
+        //get JSON data from Wiki
+        console.log(data);
+        $("#result").append(
+          '<h4>Top 15 Search results for "' + query + '"</h4>'
+        );
+        $.each(data.query.pages, function(i) {
+          var thing = data.query.pages;
+          $("#result").append(
+            '<li class="mdl-list__item"><a target="_blank" href="https://en.wikipedia.org/?curid=' +
+              thing[i].pageid +
+              '"<span class="mdl-list__item-primary-content"><i class="material-icons mdl-list__item-icon">link</i>' +
               thing[i].title +
-              "</span> </a></li>");
-    });
-
-  });
-    };
-
-
+              "</span> </a></li>"
+          );
+        });
+      }
+    );
+  }
 }
